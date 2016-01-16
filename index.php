c9978f86c633eabee4d1196a87a8928017d946d8
PWNsbey <jake.ownsbey@gmail.com>
2016-01-15 20:45:21 -0700

    adding icons

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
-	<script type="text/javascript" src="code.js"></script>
-</head>
+	<head>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+		<script type="text/javascript" src="code.js"></script>
+	</head>
 
-<body>
-	<p class="page-header" id="page-header-id">Hello, OpenShift world via PHP.</p>
-</body>
+	<body>
+		<p class="page-header" id="page-header-id">Jacob Ownsbey.</p>
+		<img src="info-xxl.png" alt="More Info" height="42" width="42">
+		<img src="literature-xxl.png" alt="Homework assignments" height="42" width="42">
+	</body>
 
 </html>
