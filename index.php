5495951abc1133457bf4bb0197a0f273b9869322
PWNsbey <jake.ownsbey@gmail.com>
2016-01-15 21:58:58 -0700

    More tweaks

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 			<p class="page-header">Jacob Ownsbey</p>
 			<p class="page-subheader">------- Hello, World.-------</p>
 			<div id="icon-div">
-				<img id="more-info-icon" src="info-xxl.png" alt="More Info" height="42" width="42">
-				<img id="homework-icon" src="literature-xxl.png" alt="Homework assignments" height="42" width="42">
+				<img id="more-info-icon" src="info-xxl.png" alt="More Info" height="60" width="60">
+				<img id="homework-icon" src="literature-xxl.png" alt="Homework assignments" height="60" width="60">
 			</div>
 		</div>
 	</body>
